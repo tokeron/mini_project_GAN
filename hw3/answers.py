6d4154b7d89ca7f330edce547a2ef11514dd9e57
Michael Toker <tok@lambda.csf.technion.ac.il>
2021-03-25 17:04:54 +0200

    wgan almost done

--- hw3/answers.py
+++ hw3/answers.py
@@ -52,13 +52,13 @@ def part3_gan_hyperparams_sngan():
 
 def part3_gan_hyperparams_wgan():
     hypers = dict(
-        batch_size=4,
+        batch_size=16,
         z_dim=64,
         data_label=1,
         label_noise=0.5,
         discriminator_optimizer=dict(
             type="RMSprop",  # Any name in nn.optim like SGD, Adam
-            lr=0.01,
+            lr=0.0001,
             # You an add extra args for the optimizer here
         ),
         generator_optimizer=dict(
